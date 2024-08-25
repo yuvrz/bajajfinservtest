a9e16f1c0f1260a56daa25c890d1786156f82dec
yuvrz <120259737+yuvrz@users.noreply.github.com>
2024-08-25 11:55:33 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/index.html');
+});
 app
   .route("/bfhl")
   .get((req, res) => {
